926b5d47e34d185c0db822ff1c889ca9ad62fd12
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-12-17 10:48:13 +0900

    socket.io メッセージ追加

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ dotenv.config()
 const host = process.env.HOST
 const port = process.env.PORT
 
+io.on('connection', (socket) => {
+    //クライアントからのデータ受信
+    socket.on('message', (data) => {
+        console.log(data)
+        //すべてのクライアントにデータ送信
+        io.emit('message', data)
+    })
+})
+
 http.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
 })
